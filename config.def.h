4ffa81d7ed3facd38c6f2994bbc32a566e0b1237
Samyak Bardiya <>
2024-01-21 20:49:10 +0530

    patch: foreground-and-background

--- config.def.h
+++ config.def.h
@@ -4,11 +4,39 @@ static const char *user = "nobody";
 static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-    [INIT] = "black",        /* after initialization */
-    [INPUT] = "#458588",     /* during input */
-    [INPUT_ALT] = "#4F7B7D", /* during input, second color */
+    [INIT] = "#458588",        /* after initialization */
+    [INPUT] = "#98971A",     /* during input */
+    [INPUT_ALT] = "#909023", /* during input, second color */
     [FAILED] = "#CC241D",    /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* insert grid pattern with scale 1:1, the size can be changed with logosize */
+static const int logosize = 50;
+/* grid width and height for right center alignment */
+static const int logow = 12;
+static const int logoh = 8;
+
+static XRectangle rectangles[9] = {
+	/* x    y       w       h */
+	{ 0,    3,      1,      3 },
+	{ 1,    3,      2,      1 },
+	{ 0,    5,      8,      1 },
+	{ 3,    0,      1,      5 },
+	{ 5,    3,      1,      2 },
+	{ 7,    3,      1,      2 },
+	{ 8,    3,      4,      1 },
+	{ 9,    4,      1,      2 },
+	{ 11,   4,      1,      2 },
+};
+
+/*Enable blur*/
+#define BLUR
+/*Set blur radius*/
+static const int blurRadius=10;
+/*Enable Pixelation*/
+//#define PIXELATIN
+/*Set pixelation radius*/
+static const int pixelSize=0;
